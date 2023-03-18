fd8d41d7f3270d971a60cc7a6a0d806eaa8a7da7
Developer kp <98082040+Kaushalw@users.noreply.github.com>
2023-03-18 22:48:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+function send(){
+var name=document.getElementById('name').value;
+var description=document.getElementById('descriptions').value;
+var mail = document.getElementById('mail').value
+        if ( document.getElementById('name'). value==='') {
+                window.navigator.vibrate('200');
+
+                alert('Enter something🔍');
+        }
+        else{
+                window.location.href='https://wa.me/919811192234/?text='+
+'Name:'+' '+name +
+'                                                         '+" Email:"
++ mail+'                                    '+ description;
  
-function onSubmit(token) {
-  document.getElementById("demo-form").submit();
+
+                
 }
- 
+}
