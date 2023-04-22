c34331d0f2d771c6885a6f675c5632f774661f44
Developer Kaushal <vk4783240@gmail.com>
2023-04-22 12:07:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-function send(){
-var name=document.getElementById('name').value;
-var email=document.getElementById('mail').value;
-var message=document.getElementById('descriptions').value;
-
-
- 
- if (  name==='') {
-                window.navigator.vibrate('200');
-
-                alert('Enter something🔍');
-        }
-        else{
-                window.location.href='https://wa.me/919811192234/?text='+'Hy'+ ' '+'I'+' '+'Am'+' '+name+' '+email +' '+message+'     '
-        }
-
-        console.log( name&&email&&message)
-}
+function send(event){
+     var name=document.getElementById('name').value;
+     var query=document.getElementById('query').value;
+     var mail=document.getElementById('mail'). value
+    // event.preventDefault();
+            
+            
+                    window.location.href='https://wa.me/919811192234/?text='
+                  + "NAME:-" + name+'                                       ' + "MAIL :-"+query+'                                     ' + "QUERY:-"+query 
+      
+    
+                    }
+    
